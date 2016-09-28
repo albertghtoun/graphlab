0d2893ade91cf532579049a6e28bcc2e852bd829
Chao Wang <albertwang87@gmail.com>
2016-09-28 00:20:04 -0700

    added code to gather a percentage of neighbors

--- run.sh
+++ run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+out="outfile.txt"
 
+echo -n "gather%%" > $out
 for file in `ls input/*.txt`
 do
-	./debug/apps/pagerank/pagerank $file
+	echo -n " $file" >> $out
+done
+echo >> $out
+
+for gp in `seq 10`
+do
+	echo `echo "0.1 * $gp" | bc` 
+	echo -n `echo "0.1 * $gp" | bc` >> $out
+	for file in `ls input/*.txt`
+	do
+		echo "    $file"
+		./debug/apps/pagerank/pagerank --infile $file --gather_percentage `echo "0.1 * $gp" | bc` 2>/dev/null | grep "runtime:" | awk '{printf(" %.2f"), $4}' >> $out
+	done
+	echo >> $out
 done
